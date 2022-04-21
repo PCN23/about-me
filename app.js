f50489c06d72bce997252b1194251c92813c309f
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-04-21 12:01:41 -0700

    added imgage elephant

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 // import functions
 
 // grab DOM elements
-const showButton = document.getElementById ('btn');
+const showButton = document.getElementById('btn');
 const animalDiv = document.getElementById('animalDiv');
 
-console.log(showButton, animalDiv);
 // set event listeners 
 
-showButton = addEventListener('click', () => {
+showButton.addEventListener('click', () => {
     animalDiv.classList.remove('hidden');
 });
 
-console.log(addEventListener);
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
